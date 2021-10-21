a79c828b4fcd5cc1aa22c6cef98987612b1930f1
jvffsantana <jvictorfig@academico.ufs.br>
2021-10-20 21:57:55 -0300

    init.sql

--- docker/dump/init.sql
+++ docker/dump/init.sql
@@ -1,6 +1,58 @@
-CREATE TABLE `backups` (
-  `backupId` int(6) NOT NULL,
-  `backupDate` varchar(20) NOT NULL,
-  `backupHour` varchar(20) NOT NULL,
-  `backupUser` varchar(30) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db:3388
+-- Tempo de geração: 21-Out-2021 às 00:38
+-- Versão do servidor: 10.6.4-MariaDB-1:10.6.4+maria~focal
+-- versão do PHP: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `Teste`
+--
+
+CREATE TABLE `Teste` (
+  `TesteId` int(10) NOT NULL,
+  `TesteName` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `Teste`
+--
+ALTER TABLE `Teste`
+  ADD PRIMARY KEY (`TesteId`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `Teste`
+--
+ALTER TABLE `Teste`
+  MODIFY `TesteId` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
